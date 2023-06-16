a7d5637e0c4b303f8b93454242ca5b31f98b07d1
Breno Angelo <brenoangeloal@gmail.com>
2023-06-15 22:48:49 -0300

    feat: Create user route

--- db/migrations/20230615011258_create_user_table.ts
+++ db/migrations/20230615011258_create_user_table.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').unique()
     table.string('name')
     table.string('email')
+    table.string('password')
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,25 @@
+import { z } from 'zod'
+import { knex } from '../database'
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'crypto'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    return 'user'
+  app.post('/', async (request, reply) => {
+    const getUsersParamsSchema = z.object({
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { email, name, password } = getUsersParamsSchema.parse(request.body)
+
+    await knex('users').insert({
+      email,
+      name,
+      password,
+      id: randomUUID(),
+    })
+
+    return reply.status(201).send()
   })
 }
